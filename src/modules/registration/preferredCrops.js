ba1217da9afa2771a59ef40af3e50a20ef6f336b
Pavan Rockeez <pavan.ravipudi@onoark.com>
2022-10-28 11:54:23 +0530

    subscription flow

--- src/modules/registration/preferredCrops.js
+++ src/modules/registration/preferredCrops.js
@@ -50,7 +50,8 @@ const PreferredCrops = () => {
         if (response.data.status.type === "SUCCESS") {
           toastr.success(response.data.status.description);
           localStorage.setItem('status',response.data.status.type);
-          navigate('/smartboard');
+          //navigate('/smartboard');
+          navigate('/plans');
         }
       },
       (error) => {
