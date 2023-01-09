537e70ad0a5b0108df22684321ffd9d288b27bac
aparna <aparna@onoark.com>
2023-01-09 19:30:15 +0530

    fixed dev issue blocker

--- src/modules/sell_bill_book/step3.js
+++ src/modules/sell_bill_book/step3.js
@@ -126,7 +126,7 @@ const SellbillStep3Modal = (props) => {
             setIncludeComm(response[i].includeInLedger == 1 ? true : false);
             setisShown(response[i].isShown == 1 ? true : false);
           } else if (response[i].settingName === "RETURN_COMMISSION") {
-            setAddRetComm(response[i].addToGt == 1 ? true : false);
+            setAddRetComm(response[i].addToGt == 1 ? fale : true);
             setIncludeRetComm(response[i].includeInLedger == 1 ? true : false);
           }
         }
