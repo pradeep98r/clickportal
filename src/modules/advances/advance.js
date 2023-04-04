be5af1298486ccee0666ca071f6bbd0139df9155
aparna <aparna@onoark.com>
2023-04-04 13:05:32 +0530

    added advance api

--- src/modules/advances/advance.js
+++ src/modules/advances/advance.js
@@ -4,11 +4,14 @@ import { getAdvances } from "../../actions/advancesService";
 import loading from "../../assets/images/loading.gif";
 import no_data_icon from "../../assets/images/NodataAvailable.svg";
 import SearchField from "../../components/searchField";
+import moment from "moment";
+import single_bill from "../../assets/images/bills/single_bill.svg";
 import {
   advanceDataInfo,
   allAdvancesData,
   totalAdvancesVal,
 } from "../../reducers/advanceSlice";
+import { getMaskedMobileNumber } from "../../components/getCurrencyNumber";
 const Advance = () => {
   const loginData = JSON.parse(localStorage.getItem("loginResponse"));
   const clickId = loginData.caId;
@@ -70,7 +73,8 @@ const Advance = () => {
             {allData.length > 0 ? (
               <div className="row">
                 <div className="col-lg-5 pl-0">
-                  <div id="search-field">
+                 <div className="row">
+                 <div id="search-field">
                     <SearchField
                       placeholder="Search by Name / Short Code"
                         onChange={(event) => {
@@ -78,8 +82,131 @@ const Advance = () => {
                         }}
                     />
                   </div>
+                 </div>
                   {advancesArray.length > 0 ? (
-                    "hey"
+                  <div>
+                  <div
+                    className="table-scroll ledger-table"
+                    id="scroll_style"
+                  >
+                    <div className="ledgers ledger_table_col">
+                      <div className="row theadr-tag p-0">
+                        <th class="col-lg-1">#</th>
+                        <th class="col-lg-2">Date</th>
+                        <th class="col-lg-6">Buyer Name</th>
+                        <th class="col-lg-3">
+                            To Be Recieved(&#8377;)
+                          </th>
+                      </div>
+                      <div>
+                        {advancesArray.map((item, index) => {
+                          return (
+                            
+                              <button
+                                // className={
+                                //   partyId == item.partyId
+                                //     ? "tabRowSelected p-0"
+                                //     : "tr-tags p-0"
+                                // }
+                                // onClick={() =>
+                                //   particularLedgerData(item.partyId, item)
+                                // }
+                              >
+                                <div className="row align-items-center">
+                                  <td className="col-lg-1">
+                                    {index + 1}
+                                  </td>
+                                  <td
+                                    key={item.date}
+                                    className="col-lg-2"
+                                  >
+                                    <p className="date_ledger_val">
+                                      {" "}
+                                      {moment(item.date).format(
+                                        "DD-MMM-YY"
+                                      )}
+                                    </p>
+                                  </td>
+                                  <td
+                                    key={item.partyName}
+                                    className="col-lg-6"
+                                  >
+                                    <div className="d-flex">
+                                      <div className="c-img">
+                                        {item.profilePic ? (
+                                          <img
+                                            className="profile-img"
+                                            src={item.profilePic}
+                                            alt="pref-img"
+                                          />
+                                        ) : (
+                                          <img
+                                            className="profile-img"
+                                            src={single_bill}
+                                            alt="img"
+                                          />
+                                        )}
+                                      </div>
+                                      <div>
+                                        <p className="namedtl-tag">
+                                          {item.partyName} -{" "}
+                                          {item.shortName}
+                                        </p>
+                                        <div className="d-flex align-items-center">
+                                          <p className="mobilee-tag">
+                                            {!item.trader
+                                              ? 'Farmmer'
+                                              : "Trader"}{" "}
+                                            - {item.partyId}&nbsp;
+                                          </p>
+                                          <p className="mobilee-tag desk_responsive">
+                                            {" | " +
+                                              getMaskedMobileNumber(
+                                                item.mobile
+                                              )}
+                                          </p>
+                                        </div>
+                                        <p className="mobilee-tag mobile_responsive">
+                                          {getMaskedMobileNumber(
+                                            item.mobile
+                                          )}
+                                        </p>
+                                        <p className="address-tag">
+                                          {item.partyAddress
+                                            ? item.partyAddress
+                                            : ""}
+                                        </p>
+                                      </div>
+                                    </div>
+                                  </td>
+                                  <td
+                                    className="col-lg-3"
+                                    key={item.tobePaidRcvd}
+                                  >
+                                    <p
+                                      className='paid-coloring'
+                                    >
+                                      789
+                                    </p>
+                                  </td>
+                                </div>
+                              </button>
+                           
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="outstanding-pay d-flex align-items-center justify-content-between">
+                  <p className="pat-tag">Outstanding Recievables:</p>
+                    <p
+                      className='values-tag'
+                    >
+                      
+                    </p>
+                  </div>
+                </div>
+
                   ) : (
                     <div className="table-scroll nodata_scroll">
                       <div className="row partner_no_data_widget_rows">
