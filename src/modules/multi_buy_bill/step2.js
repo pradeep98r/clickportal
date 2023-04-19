62a12d372a4c1d3246693177a85c3f74bee89f65
aparna <aparna@onoark.com>
2023-04-19 13:10:59 +0530

    updated table

--- src/modules/multi_buy_bill/step2.js
+++ src/modules/multi_buy_bill/step2.js
@@ -29,259 +29,47 @@ const Step2 = (props) => {
   const onclickDate = (date) => {
     setStartDate(date);
   };
-  const callbackFunction = (childData, party, type) => { };
   return (
     <div>
       <div className="main_div_padding">
         {multiSelectPartnersArray.length > 0 && (
-          <div className="crop_table_multi">
-            <div class="row head_row flex-row flex-nowrap">
-              <div class="col-lg-2">Seller</div>
-              <div class="col-lg-2">Transporter</div>
-              <div class="col-lg-1">Date</div>
-              <div class="col-lg-2">Crop</div>
-              <div class="col-lg-1">Unit type</div>
-              <div class="col-lg-1">Rate type</div>
-              <div class="col-lg-1">Number of Units</div>
-              <div class="col-lg-1">Total Weight</div>
-              <div class="col-lg-1">individual weights</div>
-              <div class="col-lg-1">Wastage</div>
-              <div class="col-lg-2">Rate (₹)</div>
-              <div class="col-lg-2">Total (₹)</div>
-            </div>
-            <div className="crop_table_view">
-              <table>
-                {multiSelectPartnersArray.map((item, index) => {
-                  return (
-                    <div class="row table_row_body table_crop_div flex-row flex-nowrap">
-                      <div class="col-lg-2">
-                        <div className="flex_class mr-0">
-                          <img
-                            src={multiSelectPartnersArray[index].profilePic}
-                            className="flex_class mr-2"
-                          />
-                          <p className="m-0">
-                            {multiSelectPartnersArray[index].partyName}
-                          </p>
-                        </div>
-                      </div>
-                      <div class="col-lg-2">
-                        <SelectPartner partyType="Transporter" parentCallback={callbackFunction} />
-                      </div>
-                      <div class="col-lg-1">
-                        <div className="d-flex align-items-center dateSelection">
-                          <span className="date_icon m-0">
-                            <img
-                              src={date_icon}
-                              alt="icon"
-                              className="dateIcon"
-                            />
-                          </span>
-                          <DatePicker
-                            dateFormat="dd-MMM-yyyy"
-                            selected={selectedDate}
-                            onChange={(date) => onclickDate(date)}
-                            className="form-control"
-                            placeholder="Date"
-                            maxDate={new Date()}
-                            onKeyDown={(e) => {
-                              e.preventDefault();
-                            }}
-                          />
-                        </div>
-                      </div>
-                      <div class="col-lg-2">tomoto</div>
-                      <div class="col-lg-1">BAGS</div>
-                      <div class="col-lg-1">Per BAG</div>
-                      <div class="col-lg-1">
-                        {" "}
-                        <input
-                          type="text"
-                          className="form-control"
-                          name="quantity"
-                        />
-                      </div>
-                      <div class="col-lg-1">2000</div>
-                      <div class="col-lg-1">individual</div>
-                      <div class="col-lg-1">-4</div>
-                      <div class="col-lg-2">250000000</div>
-                      <div class="col-lg-2">30,0000.00 + Add Crop</div>
-                    </div>
-                  );
-                })}
-              </table>
-            </div>
-          </div>
-        )}
-
-        <div style={{ width: "100%", overflowX: "auto" }}>
-          <table className="table-bordered">
-            <tr className="">
-              <td className="col-2 empty_col">hheyy jkkkkkkkkkkkkkkkk</td>
-              <td className="col-2">
-                It has survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged.
-              </td>
-              <td className="col-1">
-                {" "}
-                Lorem Ipsum is simply dummy text of the printing and
-              </td>
-              <td className="col-1">
-                {" "}
-                Lorem Ipsum is simply dummy text of the printing and
-              </td>
-              <td className="col-1">
-                {" "}
-                Lorem Ipsum is simply dummy text of the printing and
-              </td>
-              <td className="col-1">
-                {" "}
-                Lorem Ipsum is simply dummy text of the printing and
-              </td>
-              <td className="col-1">link</td>
-              <td className="col-1">jk</td>
-              <td className="col-5">
-                <div className="delete_copy_div d-flex justify-content-end">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting
-                  industry. Lorem Ipsum has been the industry's standard dummy .
-                </div>
-              </td>
-              <td className="col-1">
-                {" "}
-                Lorem Ipsum is simply dummy text of the printing and
-              </td>
-              <td className="col-2">lkkkk</td>
-              <td className="col-1">nmm</td>
-              <td className="col-1">lkkkkk</td>
-              <td className="col-5">
-                <div className="delete_copy_div d-flex justify-content-end">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting.
-                </div>
-              </td>
+          <table className="table-bordered step2_table">
+            <tr>
+              <th className="col_2">Seller</th>
+              <th className="col_2">Transporter</th>
+              <th className="col_1">Date</th>
+              <th className="col_2">Crop</th>
+              <th className="col_1">Unit type</th>
+              <th className="col_1">Rate type</th>
+              <th className="col_1">Number of Units</th>
+              <th className="col_1">Total Weight</th>
+              <th className="col_1">individual weights</th>
+              <th className="col_1">Wastage</th>
+              <th className="col_1">Rate (₹)</th>
+              <th className="col_3">Total (₹)</th>
             </tr>
+            {multiSelectPartnersArray.map((item, index) => {
+              return (
+                <tr>
+                  <td className="col_2">
+                    Select transporter Select transporter
+                  </td>
+                  <td className="col_2">asdf</td>
+                  <td className="col_1">asdfasdf</td>
+                  <td className="col_2">adfasdf</td>
+                  <td className="col_1">asdf</td>
+                  <td className="col_1">asdfasdf</td>
+                  <td className="col_1">adfasdf</td>
+                  <td className="col_1">asdf</td>
+                  <td className="col_1">asdfasdf</td>
+                  <td className="col_1">adfasdf</td>
+                  <td className="col_1">asdf</td>
+                  <td className="col_3">asdfasdf</td>
+                </tr>
+              );
+            })}
           </table>
-        </div>
-        {/* <table className="table-bordered" style={{ width: "100%" }}>
-          <tr>
-            <td style={{ width: "50%" }}>Select transporter with partner as well</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-          </tr>
-          <tr>
-            <td style={{ width: "40%" }}>Partner Selected Successfully</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-            <td style={{ width: "6.66%" }}>adfasdf</td>
-            <td style={{ width: "6.66%" }}>asdf</td>
-            <td style={{ width: "6.66%" }}>asdfasdf</td>
-          </tr>
-        </table> */}
-
-        {/* <table className="table-bordered" style={{width:"100%"}}>
-          <tr>
-            <td style={{width: "40%"}}>Select transporter</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-          </tr>
-          <tr>
-            <td style={{width: "40%"}}>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-            <td>adfasdf</td>
-            <td>asdf</td>
-            <td>asdfasdf</td>
-          </tr>
-        </table> */}
+        )}
       </div>
       <div className="bottom_div">
         <div className="d-flex align-items-center justify-content-between">
