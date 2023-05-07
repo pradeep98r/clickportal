d717d9865c1709e8ad96f963f4ec06e97e8d4184
AparnaGoldbrick <aparna@goldbrick.in>
2023-05-07 13:59:07 +0530

    updated url

--- src/pdfApi.js
+++ src/pdfApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance = axios.create({
   // 9000 for dev
   // 9001 - prod
-  baseURL: "http://54.86.67.9:9000/v1/pdfgen/",
+  baseURL: "https://dev-api.onoark.com/v1/pdfgen/",
 });
 instance.defaults.headers.common["Content-Type"] =
   "application/json;charset=UTF-8";
